ec17f60c0c7813b92a2c14695928d475eec8a87f
Kamry Henson <kamryhenson@Kamrys-Air.lan>
2021-12-06 09:39:55 -0600

    Updating files

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function buildTable(data) {
+    tbody.html("");
+
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
